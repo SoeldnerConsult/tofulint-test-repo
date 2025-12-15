77df1cd5abf718da0d02ae931ec73df97a747eba
arsiba <171977580+arsiba@users.noreply.github.com>
2025-12-15 12:50:15 +0000

    fix: resolve all issues

--- .tofulint.hcl
+++ .tofulint.hcl
@@ -4,8 +4,8 @@ plugin "opentofu" {
   source = "github.com/SoeldnerConsult/tofulint-ruleset-opentofu"
 }
 
-#plugin "google" {
-#  enabled = false
-#  version = "0.0.3"
-#  source = "github.com/SoeldnerConsult/tofulint-ruleset-opentofu"
-#}
+plugin "google" {
+  enabled = true
+  version = "0.0.3"
+  source = "github.com/SoeldnerConsult/tofulint-ruleset-google"
+}
--- google-test.tf
+++ google-test.tf
@@ -1,23 +1,23 @@
-# incorrect override strategy
-#resource "google_app_engine_domain_mapping" "domain_mapping" {
-#  domain_name       = "verified-domain.com"
-#  override_strategy = "DEFAULT"
-#
-#  ssl_settings {
-#    ssl_management_type = "AUTOMATIC"
-#  }
-#}
-#
-# invalid machine type
-#resource "google_compute_instance" "vm_instance" {
-#  machine_type = "n2-standard-1" 
-#}
-#
-# invalid member format
-#resource "google_project_iam_audit_config" "iam_audit_config" {
-#	audit_log_config {
-#		exempted_members = [
-#			"arsiba@example.com",
-#		]
-#	}
-#}
+ #incorrect override strategy
+resource "google_app_engine_domain_mapping" "domain_mapping" {
+  domain_name       = "verified-domain.com"
+  override_strategy = "STRICT"
+
+  ssl_settings {
+    ssl_management_type = "AUTOMATIC"
+  }
+}
+
+ #invalid machine type
+resource "google_compute_instance" "vm_instance" {
+  machine_type = "e2-micro" 
+}
+
+ #invalid member format
+resource "google_project_iam_audit_config" "iam_audit_config" {
+	audit_log_config {
+		exempted_members = [
+			"user:arsiba@example.com",
+		]
+	}
+}
